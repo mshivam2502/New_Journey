64f096dc2478a24feaab26633c8b02032d6a9454
mshivam2502 <118153649+mshivam2502@users.noreply.github.com>
2025-06-20 20:33:49 +0530

    RPSGame DOne

--- Python_Basics/RPSGame.py
+++ Python_Basics/RPSGame.py
@@ -1,1 +1,36 @@
-player_choice = "rock"
+import random
+
+def getChoice():
+    
+    choices = ["rock","paper","scissors"]
+    computer_choice = random.choice(choices)
+
+    return computer_choice
+
+def calculateWin(choices):
+    pc,cc = choices["pc"],choices["cc"]
+    if (pc==cc):
+        return "It's a tie"
+    elif(pc=="rock"):
+        if(cc=="paper"):
+            return "Computer Wins Player loses !!!!!"
+        elif(cc=="scissors"):
+            return "Player Wins Computer Loses 😁😁"
+    elif(pc=="paper"):
+        if(cc=="scissors"):
+            return "Computer Wins Player loses !!!!!"
+        elif(cc=="rock"):
+            return "Player Wins Computer Loses 😁😁"
+    elif(pc=="scissors"):
+        if(cc=="rock"):
+            return "Computer Wins Player loses !!!!!"
+        elif(cc=="paper"):
+            return "Player Wins Computer Loses 😁😁"
+
+    return "Error in code"
+
+choices = {"pc":input("Enter player choice: "),"cc":getChoice()}
+print("Computer's choice: " + choices["cc"] + "\nPlayer's choice: " + choices["pc"])
+print(calculateWin(choices))
+
+
