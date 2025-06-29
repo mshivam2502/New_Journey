975112d8e4b0e3119b3feb08c84bd5285392b66d
mshivam2502 <118153649+mshivam2502@users.noreply.github.com>
2025-06-29 11:53:22 +0530

    Changes in Modules.py

--- Python_Basics/Modules.py
+++ Python_Basics/Modules.py
@@ -3,3 +3,16 @@ import Classes
 roger = Classes.Dog("Roger")
 
 roger.bark()
+
+
+# math for math utilities
+# re for regular expressions
+# json to work with JSON
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for Operating System utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network requests
+# http to create HTTP servers
+# urllib to manage URLs
